ad210632efebb677681452adedaa38261b86aeee
BairImigeev <86589119+BairImigeev@users.noreply.github.com>
2022-06-21 23:24:58 +0800

    Create double_list.c

---
+++ task4/double_list.c
@@ -0,0 +1,83 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "f1.c"
+
+int main() 
+{
+    int index;
+    node* main_list;
+    int variant; 
+    FILE *file;
+	  do 
+    {
+        print_menu(); 
+        variant = get_variant(8); 
+        switch (variant) {
+            case 1:
+            int input; 
+            printf("1. Генерация случайных записей\n");
+            printf("2. Создание списка с вводом значений\n ");
+            scanf("%d", &input);
+            if (input == 1) {
+              printf("!!!");
+            }
+            if (input == 2) 
+			{   
+            node* list = NULL; 
+	      	main_list = create_list_hands(list);
+            
+	            /* printing the list. */
+			printf(" List:\n");
+			print_list(main_list);
+			printf("\n");
+            }    
+            break;
+            
+            case 2:
+                printf("введите индекс для поиска: ");
+                scanf("%d", &index);
+                printf("%d : %s ",index, (get_by_index(index, main_list))->data);
+                break;
+
+            case 3:
+                printf("введите индекс для удаления: ");
+                scanf("%d", &index);
+                main_list = remove_by_index(index, main_list);
+                print_list(main_list);
+                break;
+
+            case 4:
+                printf("введите значение для поиска : ");
+                scanf("%s", word);
+                search_value(word, main_list);
+                break;
+
+            case 5:
+                printf("введите индекс : ");
+                scanf("%d", &index);
+                printf("введите значение ");
+                scanf("%s", word);
+                main_list = add_index(word, index, main_list);
+                break;
+            
+            case 6:
+                printf("список : ");
+                print_list(main_list);
+                break;
+            case 7:
+                save_file(main_list);
+                break;
+            case 8:
+                open_file(file);
+                break;
+            case 9:
+                break;  
+        }
+       
+    } while (variant != 9);
+    
+    return 0;
+}
